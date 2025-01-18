0e32209ee36082a50a0829b8e6442a0683ace0b4
KAVALI PAVAN <145048122+KAVALIPAVAN@users.noreply.github.com>
2025-01-18 13:02:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -128,7 +128,7 @@ const App = () => {
     const username = result.value;
     setPlayerName(username);
 
-    const newSocket = io("https://tic-tac-toe-server-gamma.vercel.app/", {
+    const newSocket = io("https://tic-tac-toe-server-kappa.vercel.app/", {
       autoConnect: true,
     });
 
